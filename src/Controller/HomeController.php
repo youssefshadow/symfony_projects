b52fe2f8845abe325d3e5817731ffaecd202309f
youssefshadow <youssef20ben@gmail.com>
2023-04-12 14:11:24 +0200

    twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,10 +8,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/{nbr1}/{nbr2}', name: 'app_home')]
-    public function index($nbr1, $nbr2): Response
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
     {
-       
+        return $this->render('home/index.html.twig', [
+           
+        ]);
+    }
+
+
+    #[Route('/{nbr1}/{nbr2}', name: 'app_home_parent')]
+    public function index2($nbr1, $nbr2): Response
+    {
+        
         return $this->render('home/index.html.twig', [
             'nbr1' => $nbr1,
             'nbr2' => $nbr2,
