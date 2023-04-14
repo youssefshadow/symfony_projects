777652ac50041a18b8dbee0c1d944d0228d90a9c
youssefshadow <youssef20ben@gmail.com>
2023-04-14 15:59:10 +0200

    add_form

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ArticleRepository;
+use App\Entity\Article;
+use App\Form\ArticleType;
 
 class ArticleController extends AbstractController
 {
@@ -17,22 +19,32 @@ class ArticleController extends AbstractController
     }
 
     #[Route('/article/all', name:'app_article_all')]
-    public function showArticles(ArticleRepository $articleRepository):Response{
-        //Récupérer des article avec un tableau 
-        $articles =$articleRepository->findAll();
-        #dd($articles);
+    public function showAllArticle(ArticleRepository $articleRepository):Response{
+        //récuperer dans un tableau tous les articles
+        $articles = $articleRepository->findAll();
         return $this->render('article/index2.html.twig', [
-           'liste'=>$articles,
+            'liste'=> $articles,
         ]);
     }
-    #[Route('/article/id/{id}', name: 'app_article_id')]
-    public function showArticleById(ArticleRepository $articleRepository, $id): Response
-    {
+    #[Route('/article/id/{id}', name:'app_article_id')]
+    public function showArticleById(ArticleRepository $articleRepository, int $id):Response{
+        //récupérer l'article depuis son id
         $article = $articleRepository->find($id);
-    
+        //retourner une interface twig avec l'article récupéré
         return $this->render('article/article.html.twig', [
-            'article' => $article,
+            'article'=> $article,
         ]);
     }
+
+    #[Route('/article/add', name:'app_article_add')]
+    public function addArticle():Response{
+        
+        $article = new Article();
+
+        $form = $this->createForm(ArticleType::class, $article);
     
+        return $this->render('article/articleAdd.html.twig', [
+            'form'=> $form->createView(),
+        ]);
+    }
 }
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,20 +3,37 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\User;
+use App\Entity\Categorie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('titre')
-            ->add('contenu')
-            ->add('date')
-            ->add('categories')
-            ->add('user')
+            ->add('titre', TextType::class)
+            ->add('contenu', TextareaType::class)
+            ->add('date', DateType::class)
+            ->add('categories', EntityType::class,
+            array(
+                // looks for choices from this entity
+                'class' => Categorie::class,
+                'multiple' => true,
+                'expanded' => false,
+                'required' => false)
+            )
+            ->add('user', EntityType::class, array(
+                // looks for choices from this entity
+                'class' => User::class))
+            ->add('Ajouter', SubmitType::class)
         ;
     }
 
