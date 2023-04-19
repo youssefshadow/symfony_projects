0a5ebaf149175fc6553275ca555e21d3e88ef2a4
youssefshadow <youssef20ben@gmail.com>
2023-04-19 16:56:34 +0200

    api consctuction

--- src/Controller/ApiArticleController.php
+++ src/Controller/ApiArticleController.php
@@ -34,15 +34,39 @@ class ApiArticleController extends AbstractController
          '*'],['groups'=>'article:readbyId']);
         
     }
-    // #[Route('/api/article/add', name: 'app_api_article_add',methods:'GET')]
-    // public function addArticle(ArticleRepository $repo ,Request $request,SerializerInterface $serialize  ): Response{
-    //      $json = $request->getCContent();
-    //      $data = $serialize->decode($json, 'json');
-    //      dd($json,$data);
-    //      $article->setTitre($data['titre']);
-    //      $article->setContenu($data['contenu']);
-        //  $article->setDate( new \DatteTimeImutable $data['titre']);
-       
+    #[Route('/api/article/add', name:'app_api_article_add', methods:'PUT')]
+    public function addArticle(ArticleRepository $repo, Request $request,
+    SerializerInterface $serialize, EntityManagerInterface $em):Response{
+        //récupérer le contenu de la requête
+        $json = $request->getContent();
+        //sérializer le json en tableau
+        $data = $serialize->decode($json, 'json');
+        //instancier un objet article
+        $article = new Article();
+        //set des valeurs à l'objet
+        $article->setTitre($data['titre']);
+        $article->setContenu($data['contenu']);
+        $article->setDate(new \DateTimeImmutable($data['date']));
+        //récupérer un article
+        $recup = $repo->findOneBy(['titre'=>$data['titre'], 'date'=>$article->getDate()]);
+        if($recup){
+            //renvoyer un json
+            return $this->json(['erreur'=>'L\'article '.$article->getTitre().' existe déja en BDD'], 206, 
+            ['Content-Type'=>'application/json',
+            'Access-Control-Allow-Origin'=> 'localhost',
+            'Access-Control-Allow-Methods'=> 'GET'],[]);
+        }
+        //persister les données
+        $em->persist($article);
+        //enregistrer en BDD
+        $em->flush();
+        
+        //renvoyer un json
+        return $this->json(['erreur'=>'L\'article '.$article->getTitre().' a été ajouté en BDD'], 200, 
+        ['Content-Type'=>'application/json',
+        'Access-Control-Allow-Origin'=> 'localhost',
+        'Access-Control-Allow-Methods'=> 'GET'],[]);
+    }
          
         
     }
