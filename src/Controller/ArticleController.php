2bced8555bb1ed74c1dd2cddee3d20692896ff01
youssefshadow <youssef20ben@gmail.com>
2023-04-20 16:57:53 +0200

    json++

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -15,8 +15,11 @@ class ArticleController extends AbstractController
 {
     #[Route('/article', name: 'app_article')]
     public function index(): Response
-    {
-        return $this->render('article/index.html.twig', [
+    {    
+        
+        return $this->render('article/index2.html.twig', [
+          
+
         ]);
     }
 
