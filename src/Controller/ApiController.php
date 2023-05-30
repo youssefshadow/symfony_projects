9bb503014e9ecb5f8bc9b1ad2b018b5e0767b8b2
youssefshadow <youssef20ben@gmail.com>
2023-05-30 11:42:21 +0200

    api_controller_cnx

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
+use App\Service\ApiRegister;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class ApiController extends AbstractController
+{
+    #[Route('/api', name: 'app_api')]
+    public function index(): Response
+    {
+        return $this->render('api/index.html.twig', [
+            'controller_name' => 'ApiController',
+        ]);
+    }
+    
+    #[Route('/api/verif-connexion', name: 'verif_connexion', methods: ['GET'])]
+    public function verifConnexion(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        ApiRegister $apiRegister,
+        UserRepository $userRepository
+    ): Response{
+        // Récupérer le mail et le mot de passe depuis la requête GET
+        $email = $request->query->get('email');
+        $password = $request->query->get('password');
+
+        // Vérifier l'authentification 
+        $isAuthenticated = $apiRegister->authentification($passwordHasher, $userRepository, $email, $password);
+
+        // Retourner la réponse JSON 
+        if ($isAuthenticated) {
+            return $this->json(['connexion' => 'ok'],200,[]);
+        } else {
+            return $this->json(['connexion' => 'invalide'],400,[]);
+        }
+    }
+}
--- src/service/ApiRegister.php
+++ src/service/ApiRegister.php
@@ -1,10 +1,35 @@
 <?php
 namespace App\Service;
+
 use App\Repository\UserRepository;
 use App\Service\Utils;
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-    class ApiRegister{
-    }
+
+class ApiRegister
+{
+    public function authentification(UserPasswordHasherInterface $hash, UserRepository $repo,
+     string $mail, string $password){
+     //nettoyer avec la classe Utils et cleanInputStatic
+     $mail = Utils::cleanInputStatic($mail);
+     $password = Utils::cleanInputStatic($password);
+     //récupérer le compte utilisateur
+     $user = $repo->findOneBy(['email'=>$mail]);
+     //tester si le compte existe
+     if($user){
+         //tester le password
+         if($hash->isPasswordValid($user, $password)){
+             return true;
+          }
+          else{
+             return false;
+          }
+      }
+      //test sinon le compte n'existe pas
+      else{
+         return false;
+      }
+}
+}
 ?>
+
+
